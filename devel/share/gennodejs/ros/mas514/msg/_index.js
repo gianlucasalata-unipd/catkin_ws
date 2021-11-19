7e66c9980cd3e25c2c9c5506cb06166d54d7384b
gianlucasalata-unipd <72152971+gianlucasalata-unipd@users.noreply.github.com>
2021-11-19 12:23:38 +0100

    Add files via upload

---
+++ devel/share/gennodejs/ros/mas514/msg/_index.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+let WebJoystick = require('./WebJoystick.js');
+let ServoSetpoints = require('./ServoSetpoints.js');
+
+module.exports = {
+  WebJoystick: WebJoystick,
+  ServoSetpoints: ServoSetpoints,
+};
